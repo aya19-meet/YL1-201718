44a0f1ca15d56d133bedb88b376d02d20b6f7226
aya19-meet <aya19@meet.mit.edu>
2017-11-22 19:22:43 +0200

    uploaded lab5

---
+++ lab5/lab5.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+import random
+colormode(225)
+
+class Square(turtle):
+    def __init__(self,size):
+        Turtle.__init__(self)
+        self.shapesize(size)
+        self.shape('square')
+    
+what why how who am i doing
