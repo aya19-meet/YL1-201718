5bf48f2f3a396c6f1187d2e5452360a488a4a818
aya19-meet <aya19@meet.mit.edu>
2017-11-29 18:58:56 +0200

    pushed

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -3,10 +3,40 @@ from turtle import *
 import random
 colormode(225)
 
-class Square(turtle):
-    def __init__(self,size):
+#### Excercise 1
+
+class Square(Turtle):
+    def __init__(self,size,color):
         Turtle.__init__(self)
         self.shapesize(size)
         self.shape('square')
+        self.color(color)
+    def teleport(self):
+        self.goto(random.randint(0,100),random.randint(0,100))
+
+square1=Square(5,'blue')
+square1.teleport()
+##### Excercise 2
+
+        
+class Hexagon(Turtle):
+    def __init__(self,size):
+        Turtle.__init__(self)
+        self.begin_poly()
+        for i in range(6):
+            self.penup()
+            self.forward(5)
+            self.right(60)
+        self.end_poly()
+        self.shapesize(size)
+        register_shape('hexagon',self.get_poly())
+        self.shape('hexagon')
+
     
-what why how who am i doing
+##### Excercise 3
+
+
+
+
+
+
